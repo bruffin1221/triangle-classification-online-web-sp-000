4732b00ee74f0d85ac05d5a341c9605c306933a0
Bruce Ruffin <bruffin1221@gmail.com>
2019-12-14 21:40:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -33,20 +33,21 @@ end
 
 end
 
+#def kind(a,b,c)
+#  triangle=a+c>b && b+c>a && a+b>c && a>0 && b>0 && c>0
+#  if triangle==true && a==b && b==c && c==a
+#    :equilateral
+#  elsif triangle==true && b==c && a!=b && a!=c
+#    :isosceles
+#  elsif triangle==true && a==c && b!=c && b!=a
+#    :isosceles
+  #elsif triangle==true && a==b && b!=c && a!=c
+  #  :isosceles
+  #elsif triangle==true && a!=b && a!=c && c!=b
+  #  :scalene
+  #else triangle!=true
+  #  :invalid
+  #end
+#end
 
-def kind(a,b,c)
-  triangle=a+c>=b && b+c>=a && a+b>=c && a>0 && b>0 && c>0
-  if triangle==true && a==b && b==c && c==a
-    :equilateral
-  elsif triangle==true && b==c && a!=b && a!=c
-    :scalene
-  elsif triangle==true && a==c && b!=c && b!=a
-    :scalene
-  elsif triangle==true && a==b && b!=c && a!=c
-    :scalene
-  elsif triangle==true && a!=b && a!=c && c!=b
-    :isosceles
-  else triangle==false
-    :invalid
-  end
-end
+#kind(2,4,2)
